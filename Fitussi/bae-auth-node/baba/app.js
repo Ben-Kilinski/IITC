fd6f08d74d37aac626a917039aa9ed4ee989415e
Ben Kilinski <ben.kilinski@gmail.com>
2024-12-09 09:38:56 +0200

    boker tov typescript

--- Fitussi/bae-auth-node/baba/app.js
+++ Fitussi/bae-auth-node/baba/app.js
@@ -1,24 +1,71 @@
-const express = require('express');
-const bcrypt = require('bcrypt')
+const express = require("express");
+const bcrypt = require("bcrypt");
+
 const app = express();
+
 const PORT = 3000;
 const saltRounds = 10;
+const hashKey = "PeogG3lBZpSErxuBgrAA0Y6ermcD04XGGeTn7uUYfLvOFEvdaW";
+
+const dummyUser = {};
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.status(200).send({
-        message: "Hello from the server :_:"
-    })
-})
+app.get("/", (req, res) => {
+  res.status(200).send({
+    message: "Hello from the server!",
+  });
+});
 
-app.post('/encrypt-password', (req, res) => {
-    const { userPassword } = req.body;
-    bcrypt.hash(userPassword, saltRounds)
-})
+app.post("/sign-up", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    // console.log(userPassword);
 
+    const hashedPassword = await bcrypt.hash(password + hashKey, saltRounds);
 
+    dummyUser.email = email;
+    dummyUser.password = hashedPassword;
 
+    return res.send({
+      you: dummyUser,
+    });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.post("/sign-in", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // You task to first find the user in the DB
+
+    // Check the Email:
+    if (dummyUser.email !== email) {
+      return res.status(401).send("NO SUCH USER");
+    }
+
+    const isCorrectPassword = await bcrypt.compare(
+      password + hashKey,
+      dummyUser.password
+    );
+
+    if (isCorrectPassword) {
+      return res.status(200).send({
+        status: "success",
+        message: "Welcome",
+        data: dummyUser.email,
+      });
+    } else {
+      return res.status(401).send({
+        error: "Error",
+      });
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}`)
-})
+  console.log(`Server is running on port: ${PORT}`);
+});
