7020d9b032b6966da1ef615d68507e541185c266
Ben Kilinski <ben.kilinski@gmail.com>
2024-12-15 09:41:52 +0200

    great!

--- Fitussi/mongodb 15-12/Animals API/app.js
+++ Fitussi/mongodb 15-12/Animals API/app.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+app.use(express.json());
+
+// Routes imports
+const animalsRoutes = require('./routes/animalsRoutes.js')
 
 app.get("/", (req, res)=>{res.send('Hello Ben')})
+
+
+app.use('/animals', animalsRoutes);
+
 
 app.listen(PORT, ()=>{console.log(`Server is running in ${PORT}`)})
