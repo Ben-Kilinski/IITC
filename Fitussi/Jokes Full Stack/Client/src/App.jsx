c4c7418f8092502a63c3b3fee15652b594f73105
Ben Kilinski <ben.kilinski@gmail.com>
2024-12-16 23:28:33 +0200

    client fix

--- Fitussi/Jokes Full Stack/Client/src/App.jsx
+++ Fitussi/Jokes Full Stack/Client/src/App.jsx
@@ -1,19 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css'
-import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [joke, setJoke] = useState('')
+  const [jokes, setJokes] = useState([])
 
-  function fetchJoker () {
-    const res = awaitaxios.get('http://localhost:3000/api/v1/jokes/random')
+  async function fetchJoke () {
+    const res = await axios.get('http://localhost:3000/api/v1/jokes/random')
+    console.log(res.data)
+
+    setJoke(res.data)
+    console.log("Component updated");
   }
+
+  async function getAllJokes () {
+    const res = await axios.get('http://localhost:3000/api/v1/jokes')
+    console.log(res.data)
+    
+    setJokes(res.data)
+    console.log("Component updated");
+  }
+
+
+  useEffect(() => {
+    fetchJoke()
+    getAllJokes()
+    console.log("Component mounted");
+  }, [])
+  
   return (
     <>
-     <div></div>
-     <button on click={fetchJoker}>Get Random Joke</button>
+      {/* <div>{JSON.stringify(joke)}</div> */}
+      <div>
+        <h1>Random Joke</h1>
+        <p>{joke.setup}</p>
+        <p>{joke.punchline}</p>
+      </div>
+      <button onClick={fetchJoke}>Get Random Joke</button>
+
+      {jokes.map((joke) => (
+        <div key={joke._id}>
+          <h6>{joke.setup}</h6>
+          <p>{joke.punchline}</p>
+          <span>{joke._id}</span>
+        </div>
+      ))}
     </>
   )
 }
