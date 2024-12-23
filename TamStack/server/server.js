6fc2481308019b869a67fdb0b4494ae87f55cc56
Ben Kilinski <ben.kilinski@gmail.com>
2024-12-23 13:00:37 +0200

    GOOD DAY

--- TamStack/server/server.js
+++ TamStack/server/server.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import publicRoutes from './routes/public.js'
+
 const app = express()
-app.get('/', (request, response) => {
-    console.log('Requisição recebida:');
-    console.log(`Método: ${request.method}`);
-    console.log(`URL: ${request.url}`);
-    response.send("Server running🏃‍♂️‍➡️🚀");
-})
+app.use(express.json())
+
+
+app.use('/', publicRoutes)
+
 app.listen(3000, () => { console.log('Server is running on http://localhost:3000') })
