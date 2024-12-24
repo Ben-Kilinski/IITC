0a72d109d3386e75cac48f34fa30914cad5200b9
Ben Kilinski <ben.kilinski@gmail.com>
2024-12-24 14:42:05 +0200

    good afternon

--- Fitussi/24-12-2024/src/components/Navbar.jsx
+++ Fitussi/24-12-2024/src/components/Navbar.jsx
@@ -1,14 +1,18 @@
-const Navbar = () => {
-    return (
-        <>
-            <nav>
-                <a href="/html/">Home</a> |
-                <a href="/css/">CSS</a> |
-                <a href="/js/">JavaScript</a> |
-                <a href="/python/">Python</a>
-            </nav>
-        </>
-    )
-}
+import { Link } from 'react-router-dom'
 
-export default Navbar
+const Navbar = () => {
+return (
+    <nav style={{ background: '#333', width: "100vw", height: "7vh"}} >
+        <Link to="/" style={{ margin: '0 10px', color: '#fff' }}>Home</Link>
+        <Link to="/about" style={{ margin: '0 10px', color: '#fff' }}>About</Link>
+        <Link to="/pokemons" style={{ margin: '0 10px', color: '#fff' }}>Pokemons</Link>
+
+        {/* <Link to="/html" style={{ margin: '0 10px', color: '#fff' }}>HTML</Link>
+        <Link to="/css" style={{ margin: '0 10px', color: '#fff' }}>CSS</Link>
+        <Link to="/js" style={{ margin: '0 10px', color: '#fff' }}>JavaScript</Link>
+        <Link to="/python" style={{ margin: '0 10px', color: '#fff' }}>Python</Link> */}
+    </nav>
+);
+};
+
+export default Navbar;
--- Fitussi/24-12-2024/src/main.jsx
+++ Fitussi/24-12-2024/src/main.jsx
@@ -1,19 +1,27 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { BrowserRouter, Routes, Route } from 'react-router'
-import Home from './pages/Home.jsx'
-import About from './pages/About.jsx'
-import Navbar from './components/Navbar.jsx'
-import NotFound from './pages/NotFound.jsx'
+
+// Pages
+import HomePage from './pages/HomePage'
+import AboutPage from './pages/AboutPage'
+import PokemonPage from './pages/PokemonPage'
+import NotFound from './pages/NotFound'
+
+// Components
+import Navbar from './components/Navbar'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-  <Navbar />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-
-      <Route path="*" element={<NotFound />} /> 
-    </Routes>
-  </BrowserRouter>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/about' element={<AboutPage/>} />
+        <Route path='/pokemons' element={<PokemonPage />} >
+          <Route path="yoav" element={<h1>Yoav Header</h1>}/>
+          <Route path=":id" element={<h1>Test Header</h1>}/>
+        </Route>
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
 )
