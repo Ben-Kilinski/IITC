ca03a5ad12951372991fd541ff76396f7c8630cc
Ben Kilinski <ben.kilinski@gmail.com>
2024-12-24 15:42:21 +0200

    baba

--- Fitussi/24-12-2024/src/main.jsx
+++ Fitussi/24-12-2024/src/main.jsx
@@ -12,16 +12,16 @@ import NotFound from './pages/NotFound'
 import Navbar from './components/Navbar'
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/about' element={<AboutPage/>} />
-        <Route path='/pokemons' element={<PokemonPage />} >
-          <Route path="yoav" element={<h1>Yoav Header</h1>}/>
-          <Route path=":id" element={<h1>Test Header</h1>}/>
-        </Route>
-        <Route path='*' element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+  <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/about' element={<AboutPage />} />
+      <Route path='/pokemons' element={<PokemonPage />} >
+        <Route path="yoav" element={<h1>Yoav Header</h1>} />
+        <Route path=":id" element={<h1>Test Header</h1>} />
+      </Route>
+      <Route path='*' element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
 )
--- Fitussi/24-12-2024/src/pages/AboutPage.jsx
+++ Fitussi/24-12-2024/src/pages/AboutPage.jsx
@@ -1,13 +1,6 @@
 const AboutPage = () => {
     return (
         <>
-            <img src="https://64.media.tumblr.com/53f44b02ee495e8f4f1e3a98d01d90c1/a43f17567e70751f-d3/s540x810/bba3126b32f8906ee0da3512c7d4a946e4f17b6f.gif"
-                style={{
-                    width: '100%', // Largura ocupando toda a tela
-                    height: '16%', // Ajusta a altura proporcionalmente
-                    maxHeight: '200px', // Limita a altura máxima (ajuste conforme necessário)
-                    objectFit: 'cover', // Garante que a imagem preencha o espaço sem distorção
-                }} />
             <h1>This is About page🧙‍♂️</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste consectetur dolorum quasi delectus quod at corrupti, quisquam animi tempore deleniti aperiam ea voluptates amet debitis blanditiis ullam nisi sequi non quaerat doloremque vero. Nulla atque sint accusamus veritatis omnis magni quae eius iste tempore voluptatibus voluptate blanditiis, officiis autem. Ducimus quod reiciendis nihil rerum placeat quaerat eius. Dolor numquam ipsum sint facere commodi modi! Asperiores odio esse aliquid ratione delectus, odit laborum sapiente vel quibusdam! Accusantium, magni dignissimos quae ut quia vitae repellat provident voluptatem odit consequuntur. Facilis nulla at illum eius obcaecati, soluta iusto? Minima a incidunt alias exercitationem!</p>
         </>
