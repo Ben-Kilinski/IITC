678dc96fa632cae3d1451357f6932a83caaf3fcd
Ben Kilinski <ben.kilinski@gmail.com>
2024-12-24 10:26:03 +0200

    boker tov 3

---
+++ Fitussi/24-12-2024/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <>
+            <nav>
+                <a href="/html/">Home</a> |
+                <a href="/css/">CSS</a> |
+                <a href="/js/">JavaScript</a> |
+                <a href="/python/">Python</a>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
--- Fitussi/24-12-2024/src/main.jsx
+++ Fitussi/24-12-2024/src/main.jsx
@@ -1,12 +1,19 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router'
-import Home from './assets/pages/Home.jsx'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Navbar from './components/Navbar.jsx'
+import NotFound from './pages/NotFound.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+  <Navbar />
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+
+      <Route path="*" element={<NotFound />} /> 
     </Routes>
   </BrowserRouter>
 )
