437ca357ed2bf9cd0a567f760a658241345afdff
Ben Kilinski <ben.kilinski@gmail.com>
2024-12-24 10:02:36 +0200

    boker tov2

---
+++ Fitussi/24-12-2024/src/assets/pages/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <>
+            <h1>This is home page👌</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste consectetur dolorum quasi delectus quod at corrupti, quisquam animi tempore deleniti aperiam ea voluptates amet debitis blanditiis ullam nisi sequi non quaerat doloremque vero. Nulla atque sint accusamus veritatis omnis magni quae eius iste tempore voluptatibus voluptate blanditiis, officiis autem. Ducimus quod reiciendis nihil rerum placeat quaerat eius. Dolor numquam ipsum sint facere commodi modi! Asperiores odio esse aliquid ratione delectus, odit laborum sapiente vel quibusdam! Accusantium, magni dignissimos quae ut quia vitae repellat provident voluptatem odit consequuntur. Facilis nulla at illum eius obcaecati, soluta iusto? Minima a incidunt alias exercitationem!</p>
+        </>
+    )
+};
+
+export default Home
