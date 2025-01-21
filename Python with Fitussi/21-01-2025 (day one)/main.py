d58e496ebc2f166643ded79682c73fd69aa53826
Ben Kilinski <ben.kilinski@gmail.com>
2025-01-21 14:34:27 +0200

    kug

--- Python with Fitussi/21-01-2025 (day one)/main.py
+++ Python with Fitussi/21-01-2025 (day one)/main.py
@@ -35,14 +35,47 @@
 
 # get user name, age and 2 numbers returning from them a rectangle area
 
-name = input("What's your name?")
-age = int(input("How old are you?"))
-print(f"Hello {name}")
+# name = input("What's your name?")
+# age = int(input("How old are you?"))
+# print(f"Hello {name}")
 
 
-print("Now {name}, please insert a height and a width and i will calculate the area of a rectangle")
-h = input(("Height: "))
-w = input(("Width: "))
-h = int(h)
-w = int(w)
-print(f"Rectangle area = {h*w} cm2")
+# print("Now {name}, please insert a height and a width and i will calculate the area of a rectangle")
+# h = input(("Height: "))
+# w = input(("Width: "))
+# h = int(h)
+# w = int(w)
+# print(f"Rectangle area = {h*w} cm2")
+
+# a = input("insert a number: ")
+# b = input("another one: ")
+# a = int(a)
+# b = int(b)
+# print(f"{a} x {b} = {a*b}\n{a} / {b} = {a/b}\n{a} + {b} = {a+b}\n6{a} - {b} = {a-b}")
+
+# checking if a number is positive, negative or zero
+# num = input("insert a number: ")
+# num = int(num)
+# if num > 0:
+#     print("Positive number")
+# elif num == 0:
+#     print("neutral number")
+# else: 
+#     print("Negative number")
+
+# checking leaps years
+# O ano deve ser divisível por 4.
+# Se o ano for divisível por 100, ele também deve ser divisível por 400 para ser considerado bissexto.
+# Caso contrário, o ano não será bissexto.
+
+# year = int(input("Choose a year: "))
+# if year % 4 == 0 and year % 100 != 0 or year%100==0 and year%400==0:
+#     print("thats a leap year, congrats!")
+# else:
+#     print("try again")
+
+numbers = [1, 33, 5, 6, 3, 7, 8, 22, 44, 55, 66, 99, 88, 63, 34, 7, 0, 2]
+total = 0 
+for i in range (10):
+    total =  +numbers[i]
+print(total)
