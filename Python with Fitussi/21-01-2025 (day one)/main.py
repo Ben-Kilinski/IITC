540125509be14f202a36455f2eba51792a11d0ef
Ben Kilinski <ben.kilinski@gmail.com>
2025-01-21 13:30:34 +0200

    Learning Python

--- Python with Fitussi/21-01-2025 (day one)/main.py
+++ Python with Fitussi/21-01-2025 (day one)/main.py
@@ -1,2 +1,48 @@
 # good moorning, Python is awesome!
 
+# print("Helllo Ben, welcome to python 🦆")
+
+# '''My project is terrible, beacuse i forgot how to write awesome'''
+
+
+# program to calculate the total cost of 3 items 
+
+# pen = 8
+# notebook = 15
+# pencilcase = 18
+
+# def print_result(a, b, c):
+    # result = int(a + b + c)
+    # print(result)
+
+# print_result(pen, notebook, pencilcase)
+
+
+# check if a number is within a range
+
+# def is_in_range():
+#     userAge = input("How old are you?")
+#     age = int(userAge) 
+#     if 18 < age < 48:
+#         print("Yes, you can join us")
+#     elif 18 > age:
+#         print("Sorry, yo´re too young :(")
+#     elif age > 48:
+#         print("Sorry, yo´re too old for this :(")
+
+# is_in_range()
+
+
+# get user name, age and 2 numbers returning from them a rectangle area
+
+name = input("What's your name?")
+age = int(input("How old are you?"))
+print(f"Hello {name}")
+
+
+print("Now {name}, please insert a height and a width and i will calculate the area of a rectangle")
+h = input(("Height: "))
+w = input(("Width: "))
+h = int(h)
+w = int(w)
+print(f"Rectangle area = {h*w} cm2")
