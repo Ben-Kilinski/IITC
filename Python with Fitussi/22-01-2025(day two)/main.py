625c708aa6e59ccc32ab12f06e3e591af523123d
Ben Kilinski <ben.kilinski@gmail.com>
2025-01-22 09:20:52 +0200

    boker tov

---
+++ Python with Fitussi/22-01-2025(day two)/main.py
@@ -0,0 +1,5 @@
+movies = ["Jurassic Park", "Indiana Jones", "Back to the Future", "Pirates of the Carebbean", "Harry Potter"]
+movies.append("American Sniper")
+print(movies)
+movies.remove("Pirates of the Carebbean")
+print(movies)
