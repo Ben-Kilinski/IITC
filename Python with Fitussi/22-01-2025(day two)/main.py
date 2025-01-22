aba7a8caf6d17b37cb01d0d8c5d6a1d44746f55a
Ben Kilinski <ben.kilinski@gmail.com>
2025-01-22 09:54:16 +0200

    bb

--- Python with Fitussi/22-01-2025(day two)/main.py
+++ Python with Fitussi/22-01-2025(day two)/main.py
@@ -1,5 +1,25 @@
-movies = ["Jurassic Park", "Indiana Jones", "Back to the Future", "Pirates of the Carebbean", "Harry Potter"]
-movies.append("American Sniper")
-print(movies)
-movies.remove("Pirates of the Carebbean")
-print(movies)
+# movies = ["Jurassic Park", "Indiana Jones", "Back to the Future", "Pirates of the Carebbean", "Harry Potter"]
+# movies.append("American Sniper")
+# print(movies)
+# movies.remove("Pirates of the Carebbean")
+# print(movies)
+
+#tuple
+# birthday = (10, 3, 1997)
+# birthday.remove(3)
+# print(birthday)
+
+ben = {
+    "age" : 27, 
+    "name" : "Ben", 
+    "lname" : "Kilinski", 
+    "occupation" : "student", 
+    "sibus" : False
+}
+
+
+print(ben['age'])
+ben["book"] = "Harry Potter"
+print(ben)
+del ben["sibus"]
+print(ben)
