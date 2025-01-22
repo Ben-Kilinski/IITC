a6c08b7b4ca5cdf4893dd55aa611f29172d76220
Ben Kilinski <ben.kilinski@gmail.com>
2025-01-22 11:25:52 +0200

    list ot tuple

--- Python with Fitussi/22-01-2025(day two)/main.py
+++ Python with Fitussi/22-01-2025(day two)/main.py
@@ -9,17 +9,42 @@
 # birthday.remove(3)
 # print(birthday)
 
-ben = {
-    "age" : 27, 
-    "name" : "Ben", 
-    "lname" : "Kilinski", 
-    "occupation" : "student", 
-    "sibus" : False
-}
+# ben = {
+#     "age" : 27, 
+#     "name" : "Ben", 
+#     "lname" : "Kilinski", 
+#     "occupation" : "student", 
+#     "sibus" : False
+# }
 
 
-print(ben['age'])
-ben["book"] = "Harry Potter"
-print(ben)
-del ben["sibus"]
-print(ben)
+# print(ben['age'])
+# ben["book"] = "Harry Potter"
+# print(ben)
+# del ben["sibus"]
+# print(ben)
+
+# ben = {"football", "guitar", "hamburguer"}
+# sabag = {'sibus', " running", "computers", "hamburguer", "ktzitzot"}
+# shared_hobbies = set()
+# for i in ben:
+#     for j in sabag:
+#         if i == j:
+#             shared_hobbies.add(i)
+
+
+# print(shared_hobbies)
+
+# colors = ["red", "blue", "yellow", "orange", "black"]
+# print(colors[0])
+# print(colors[-1])
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# for i in numbers:
+#     if i % 2 == 0:
+#         print(i)
+
+colors_list = ["red", "blue", "yellow", "orange", "black"] 
+print(colors_list)
+colors_tuple = tuple(colors_list)
+print(colors_tuple)
